31ef06ff5954285bf342c7772cdf49ec188948f6
imyoubin <imyoubin@hanmail.net>
2025-05-29 09:45:01 +0900

    2222222222222

--- Ex/day05.sql
+++ Ex/day05.sql
@@ -153,26 +153,30 @@ from departments d left outer join  employees e
 -- ------------------------------
 # full outer join    union
 -- ------------------------------
-(-- left조인
-select 	e.employee_id,
-		e.first_name,
-		d.department_id,
-		d.department_name
-from employees e
-left outer join departments d
-	on e.department_id = d.department_id
-)
-union
-(-- right조인
-select 	e.employee_id,
-		e.first_name,
-		d.department_id,
-		d.department_name
-from employees e
-right outer join departments d
-	on e.department_id = d.department_id
-) 
-;
+
+select count(*)
+from (t);
+(
+	(-- left조인
+	select 	e.employee_id,
+			e.first_name,
+			d.department_id,
+			d.department_name
+	from employees e
+	left outer join departments d
+		on e.department_id = d.department_id
+	)
+	union
+	(-- right조인
+	select 	e.employee_id,
+			e.first_name,
+			d.department_id,
+			d.department_name
+	from employees e
+	right outer join departments d
+		on e.department_id = d.department_id
+	) 
+);
 
 -- 왼쪽기준
 select 	e.employee_id,
