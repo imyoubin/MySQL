4a14225e72f36f3d1bbd8779c83234583a0661a4
imyoubin <imyoubin@hanmail.net>
2025-06-03 17:07:33 +0900

    2025.06.03

--- Ex/web01.sql
+++ Ex/web01.sql
@@ -1,9 +1,8 @@
 -- ----------------------------------------------------
--- web 계정 --01
+-- web 계정 - 01 (기본테이블, insert문 중심)
 -- ----------------------------------------------------
 
 -- #테이블 만들기
-
 create table book(
 	book_id int, 
     title varchar(50), 
@@ -28,7 +27,7 @@ rename table book to article;
 -- #테이블 삭제
 drop table article;
 
--- select * from article;
+select * from article;
 
 
 -- #작가 테이블 만들기
@@ -46,25 +45,26 @@ values(1, '박경리', '토지작가');
 */
 insert into author 
 values(1, '박경리', '토지작가');
-/*
+
 insert into author     -- 오류
 values(2, '이문열');   -- 컬럼수와 데이터 수가 맞지 않음 
-*/
-insert into author         -- 데이터 갯수를 맞쳐야 함
-values(7, '이문열', '');   -- 작가설명에 '' 가짜글자 데이터를 입력한것임
 
-insert into author values(6, '박명수', null);         -- 데이터 갯수를 맞쳐야 함	-- 작가설명에 null 실제데이터가 없는값
- 
+insert into author         -- 데이터 갯수를 맞쳐야 함
+values(2, '이문열', '');   -- 작가설명에 '' 가짜글자 데이터를 입력한것임
+
+insert into author          -- 데이터 갯수를 맞쳐야 함
+values(6, '박명수', null);  -- 작가설명에 null 실제데이터가 없는값
+
 insert into author
-values(3, '임유빈', '강사');
+values(3, '황일영', '강사');
 
 -- 컬럼 명을 표시하면 데이터가 컬럼명과 일치해야 한다
-insert into author(author_id, author_name)  values(4, '정우성');-- 정상
+insert into author(author_id, author_name)   -- 정상
+values(4, '정우성');
 
-/*
 insert into author(author_id, author_desc)   -- 오류 author_name --> not null   
 values(5, '런닝맨');                         -- author_name 데이터가 꼭 있어야 한다  
-*/
+
 insert into author(author_name, author_desc) -- 오류 author_id --> pk (unique + not null)   
 values('유재석', '런닝맨');                  -- author_id 데이터가 꼭 있어야 한다(유니크해야한다)  
 
